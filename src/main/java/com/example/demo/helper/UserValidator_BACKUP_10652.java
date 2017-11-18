3215c8b36664f119cbf2b9f6ccaaf27f1be9b2f4
Kartik Sharma <kartik.sharma1607@outlook.com>
2017-11-18 19:44:15 +0530

    MySql based configuration

---
+++ src/main/java/com/example/demo/helper/UserValidator_BACKUP_10652.java
@@ -0,0 +1,64 @@
+package com.example.demo.helper;
+
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.Period;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.example.demo.models.User;
+import com.example.demo.services.UserService;
+
+@Component
+public class UserValidator implements Validator {
+	
+	@Autowired
+    private UserService userService;
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return User.class.equals(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		User user = (User) target;
+		if (user.getName().length() < 6 || user.getName().length() > 32) {
+            errors.rejectValue("name", "Size.userForm");
+        }
+<<<<<<< HEAD
+=======
+
+>>>>>>> a1ba6a22aa1c9934fa57a4c40ae47612832fa52a
+		String nameRegex = "^[\\p{L} .'-]+$";
+		if ( ! user.getName().matches(nameRegex)) {
+			errors.rejectValue("name", "Invalid.userForm");
+		}
+<<<<<<< HEAD
+		
+=======
+>>>>>>> a1ba6a22aa1c9934fa57a4c40ae47612832fa52a
+		
+		BigInteger contactNumber = new BigInteger(user.getContactNumber());
+		if ( user.getContactNumber().length() != 10 || contactNumber.signum() == -1) {
+			errors.rejectValue("contactNumber", "Size.userForm");
+		} else if( userService.findByContactNumber(user.getContactNumber()).isSuccess()) {
+			errors.rejectValue("contactNumber", "Duplicate.userForm");
+		}
+		
+		if ( userService.findByEmailAddress(user.getEmailAddress()).isSuccess() ) {
+			errors.rejectValue("emailAddress", "Duplicate.userForm");
+		}
+		
+		LocalDate birthdate = user.getDateOfBirth().toLocalDate();
+		LocalDate now = LocalDate.now();
+		int age = Period.between(birthdate, now).getYears();
+		if ( age < 18 ) {
+			errors.rejectValue("dateOfBirth", "Size.userForm");
+		}
+	}
+
+}
